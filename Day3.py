9ecefc2ef9cb114585b2e1e3f0b4bc0d31761f84
alanoudmohmmed <anood_225@hotmail.com>
2019-08-24 06:32:58 +0300

    day3

---
+++ Day3.py
@@ -0,0 +1,47 @@
+x= 5
+y= "4"
+print(x)
+
+
+print(y)
+z = 4 # z this is of type int
+z= "value" # z is now of type string
+print(z)
+
+
+z= "num"
+print(z)
+# is the same as
+z= 'num'
+print(z)
+
+s , w, e = "ahmmed" , "mohmmed" , "naif"
+print(s)
+print(w)
+print(e)
+
+
+r = t = u = "name"
+print(r)
+print(t)
+print(u)
+
+k="simple"
+print("python is :"+ k)
+
+p="100Day"
+l= "OfCode"
+o= p+l 
+print(o)
+
+h=100
+f=50
+g = h+f
+print(g)
+
+# البايثون لاتسمح لك بين جمع رقم ونص
+
+j= "hello"
+c= 5
+b = j+c
+print(b)
